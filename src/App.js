9310f467c47c8810a273563effa969958d56da5e
Amirtharaj-R <amirtharaj@thecareerlabs.com>
2022-11-15 18:27:24 +0530

    update second section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import "./App.css";
 function App() {
   return (
-    <section className="bannersection">
-      <div>
-        <div className="GRE-Head">
-          <img src="Assets/Careerlabs-logo.png" alt="" />
-        </div>
-        <div className="bannersection-col">
-          <div className="bannercontent">
-            <p>Empower your Career with the GRE Exam!</p>
+    <>
+      <section className="bannersection">
+        <div>
+          <div className="GRE-Head">
+            <img src="Assets/Careerlabs-logo.png" alt="" />
           </div>
-          <div>
-            <img className="bannerimg" src="Assets/Gre-banner-img.png" alt="" />
+          <div className="bannersection-col">
+            <div className="bannercontent">
+              <p>Empower your Career with the GRE Exam!</p>
+            </div>
+            <div>
+              <img
+                className="bannerimg"
+                src="Assets/Gre-banner-img.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section class="second-sec">
+        <div class="second-sec-content">
+          <p>
+            General Record Examination (GRE) - One of the world’s largest
+            assessment programs for all the aspiring candidates looking to
+            pursue masters' Studies and Ph.D. abroad. Created and administered
+            by ETS (Educational Testing System), the GRE exam consists of the
+            General Test and Subject Test. Generally, a candidate is required to
+            attempt the GRE General Test, while Subject Tests are taken
+            additionally to display expertise in that domain according to the
+            target's school requirements. The GRE exam is available in three
+            formats - Computer-based, Paper-based and Home-based to assess the
+            candidates' analytical, verbal and quantitative skills.
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
 
