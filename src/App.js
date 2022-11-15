f30770bc980020c80c129f2531594f94f0df3155
Amirtharaj-R <amirtharaj@thecareerlabs.com>
2022-11-15 18:35:06 +0530

    third section update

--- src/App.js
+++ src/App.js
@@ -37,6 +37,131 @@ function App() {
           </p>
         </div>
       </section>
+      <section class="third-sec">
+        <div class="third-sec-pad">
+          <div class="third-sec-head">
+            <p>Master the science behind the GRE Sections</p>
+          </div>
+          <div class="third-sec-keyd">
+            <div class="third-sec-keyd-line">
+              <p>Key Details</p>
+            </div>
+          </div>
+          <div class="third-sec-dot-sec">
+            <div class="third-sec-Dots">
+              <img src="Assets/third-sec-dots.png" alt="" />
+            </div>
+            <div class="third-sec-Dots-col4">
+              <div class="third-sec-Dots-col4-cols">
+                <p>Exam Fee</p>
+                <h4>USD 205</h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols">
+                <p>Duration</p>
+                <h4>3Hr 45Min</h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols">
+                <p>Eligibility</p>
+                <h4>
+                  None, except
+                  <br />
+                  Bachelor’s Degree
+                </h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols">
+                <p>Where</p>
+                <h4>Nearby Test Centre</h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols">
+                <p>Medium</p>
+                <h4>Computer-based</h4>
+              </div>
+            </div>
+            <div class="third-sec-Dots">
+              <img src="Assets/third-sec-dot-2.png" alt="" />
+            </div>
+            <div class="third-sec-Dots-col4-2">
+              <div class="third-sec-Dots-col4-cols">
+                <p>Registration</p>
+                <h4>On ETS Website</h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols">
+                <p>Max. Score</p>
+                <h4>340</h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols third-sec-Dots-col4-col3">
+                <p>Sections</p>
+                <h4>
+                  &#9679; Analytical Writing Assessment
+                  <br />
+                  &#9679; Verbal Reasoning
+                  <br />
+                  &#9679; Verbal Reasoning
+                </h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols third-sec-Dots-col4-col4">
+                <p>Score Validity</p>
+                <h4>5 Years</h4>
+              </div>
+              <div class="third-sec-Dots-col4-cols third-sec-Dots-col4-col5">
+                <p>GRE Subject Tests</p>
+                <h4>
+                  Biology
+                  <br />
+                  Chemisty
+                  <br />
+                  English Literature
+                  <br />
+                  Mathematics
+                  <br />
+                  Physics
+                  <br />
+                  Psychology
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div class="third-sec-dot-sec-mob-view">
+            <div class="mobile-dot-sec-img">
+              <img src="Assets/Dot-sec-mobile-img.png" alt="" />
+            </div>
+          </div>
+          <div class="third-sec-content">
+            <div class="third-sec-Arrow-img">
+              <img src="Assets/Third-sec-bullet-img.png" alt="" />
+            </div>
+            <div class="third-sec-bullet-cont">
+              <p>
+                A maximum score of 340 can be achieved in the exam. The GRE
+                General Test comprises 3 major sections
+              </p>
+              <ol type="1">
+                <li>Analytical Writing</li>
+                <li>Verbal Reasoning</li>
+                <li>Quantitative Reasoning</li>
+              </ol>
+            </div>
+          </div>
+          <div class="third-sec-content">
+            <div class="third-sec-Arrow-img">
+              <img src="Assets/Third-sec-bullet-img.png" alt="" />
+            </div>
+            <div class="third-sec-bullet-cont">
+              <p>
+                For the GRE Subject Test, the candidates are required to appear
+                for any one of the following subjects according to the school’s
+                requirement:
+              </p>
+              <ol type="1">
+                <li>Chemistry</li>
+                <li>Mathematics</li>
+                <li>Physics</li>
+                <li>Psychology</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
